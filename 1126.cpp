202a291fc7e05a51ac23218b3613c2ee53de3ea2
zealtez <zealtez@naver.com>
2017-06-19 11:59:49 +0900

    Create 1126.cpp

---
+++ 1126.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int inf = 10000000;
+int a[50];
+int d[50][250001];
+int n;
+int go(int k, int diff) {
+    if (diff > 250000) {
+        return -inf;
+    }
+    if (k == n) {
+        if (diff == 0) {
+            return 0;
+        } else {
+            return -inf;
+        }
+    }
+    int &ans = d[k][diff];
+    if (ans != -1) {
+        return ans;
+    }
+    ans = go(k+1, diff);
+    ans = max(ans, go(k+1, diff+a[k]));
+    if (a[k] > diff) {
+        ans = max(ans, diff + go(k+1, a[k]-diff));
+    } else {
+        ans = max(ans, a[k] + go(k+1, diff-a[k]));
+    }
+    return ans;
+}
+int main() {
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    memset(d,-1,sizeof(d));
+    int ans = go(0, 0);
+    if (ans == 0) {
+        cout << -1 << '\n';
+    } else {
+        cout << ans << '\n';
+    }
+    return 0;
+}
