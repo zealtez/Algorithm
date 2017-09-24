47ee92abcd534c9d8873a2b32bc17d37272cbbc8
zealtez <zealtez@naver.com>
2017-09-25 00:39:59 +0900

    Create 5339.java

---
+++ 5339.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    AL5339(args);
+    }
+    public static void AL5339(String[] args){
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\ ");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
